9b56d05889fc33ed2922547128872375217f9ff3
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2023-09-19 09:09:00 +0100

    change app.ts configuration

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+
+import * as config from "./config";
 
 const app = express();
-const port = 3000;
+
+app.set("port", config.PORT);
 
 app.use(helmet());
+app.disable("x-powered-by");
 
-app.get('/', function(req, res) {
-    res.send('Hello World')
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", function(req, res) {
+    res.send("Hello World");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running http://localhost:${port}`)
-})
+app.listen(config.PORT, () => {
+    console.log(`Server is running http://localhost:${config.PORT}`);
+});
