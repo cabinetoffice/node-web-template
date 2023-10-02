5c8033ec3ce300ed65922cafeef54365351cfdb0
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2023-10-02 09:20:27 +0100

    add nunjucks and helmet config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,4 @@
 export const PORT = process.env["PORT"] || "3000";
+export const LANDING_PAGE = "index";
+export const CDN_HOST = "d6nh3dxv55e16.cloudfront.net";
+export const JQUERY_URL = "code.jquery.com";
