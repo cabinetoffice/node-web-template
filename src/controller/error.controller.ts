74b6f7f33909512264d0f9e069cebf60adc9a979
Mouhajer-CO <mouhajer.zaouali@digital.cabinet-office.gov.uk>
2023-10-05 08:06:16 +0100

    Update controller folder and add healthcheck logic

--- src/controller/error.controller.ts
+++ src/controller/error.controller.ts
@@ -3,11 +3,11 @@ import { NextFunction, Request, Response } from "express";
 import * as config from "../config";
 import { ErrorLogMessage } from "../model";
 
-export const errorNotFound = (req: Request, res: Response) => {
+export const errorNotFound = (_req: Request, res: Response) => {
     return res.status(404).render(config.NOT_FOUND);
 };
 
-export const errorHandler = (err: Error, req: Request, res: Response, _next: NextFunction) => {
+export const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
     const statusCode = (!res.statusCode || res.statusCode === 200) ? 500 : res.statusCode;
     const errorMessage = err.message || "An error has occured. Re-routing to the error screen";
     const error: ErrorLogMessage = { code: statusCode, message: errorMessage };
