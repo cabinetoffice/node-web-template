8a72a9bbe8240338ffd61b3b67db4215ee25230b
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2023-10-12 17:28:06 +0100

    add http and https server tests

---
+++ test/unit/utils/createHttpServer.spec.ts
@@ -0,0 +1,24 @@
+import { createHttpServer } from '../../../src/utils/createHttpServer' 
+import * as http from 'http';
+import { Express } from 'express';
+
+jest.mock('express');
+
+jest.mock('http', () => ({
+  createServer: jest.fn()
+}));
+
+describe('HTTP server tests ', () => {
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+    
+    test('should return an http.Server instance', () => {
+
+        const app = {} as Express;
+        createHttpServer(app);
+        expect(http.createServer).toBeCalled();
+        expect(http.createServer).toBeCalledWith(app);
+  });
+});
---
+++ test/unit/utils/createHttpsServer.spec.ts
@@ -0,0 +1,34 @@
+import { createHttpsServer } from '../../../src/utils/createHttpsServer' 
+import * as https from 'https';
+import { Express } from 'express';
+
+jest.mock('express');
+
+jest.mock('fs', () => ({
+  readFileSync: jest.fn().mockReturnValue('')
+}));
+
+jest.mock('https', () => ({
+  createServer: jest.fn()
+}));
+
+describe('HTTPS server test', () => {
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+    
+    test('should return an https.Server instance', () => {
+        
+        const app = {} as Express;
+        const options = {
+          key: '',
+          cert: ''
+      };
+      
+        createHttpsServer(app);
+        expect(https.createServer).toBeCalled();
+        expect(https.createServer).toBeCalledWith(options, app);
+
+  });
+});
