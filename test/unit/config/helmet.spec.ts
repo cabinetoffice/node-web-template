4de3799e599e7634ddf8598221fc2deb74c36350
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2024-02-20 15:27:52 +0000

    update helmet and cors config unit tests

---
+++ test/unit/config/helmet.spec.ts
@@ -0,0 +1,27 @@
+jest.mock('helmet');
+
+import { describe, expect, test, jest, afterEach } from '@jest/globals';
+import express from 'express';
+import helmet from 'helmet';
+
+import { configureHelmet } from '../../../src/config/helmet';
+import { MOCK_HELMET_VALUE } from '../../mock/data';
+
+describe('Helmet Middleware test suites', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    test('Should call helmet methos and express app.use method', () => {
+        const mockHelmet = helmet as unknown as jest.Mock;
+        const mockApp = {
+            use: jest.fn()
+        } as unknown as express.Application;
+
+        configureHelmet(mockApp);
+
+        expect(mockHelmet).toHaveBeenCalledTimes(1);
+        expect(mockHelmet).toHaveBeenCalledWith(MOCK_HELMET_VALUE);
+        expect(mockApp.use).toHaveBeenCalled();
+    });
+});
