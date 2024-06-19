9f108468f7c1f0d66ca4f87961b78607ce779b34
mel-am <122634403+mel-am@users.noreply.github.com>
2024-06-19 15:16:08 +0100

    Updated eslint

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -1,28 +1,27 @@
-/* eslint-disable indent */
-/* eslint-disable @typescript-eslint/quotes */
-import { NextFunction, Request, Response } from "express";
-import { log } from "../utils/logger";
-import { isFeatureEnabled } from "../utils/isFeatureEnabled";
-import * as config from "../config";
+import { NextFunction, Request, Response } from 'express';
+import { log } from '../utils/logger';
+import { isFeatureEnabled } from '../utils/isFeatureEnabled';
+import * as config from '../config';
 
 export const authentication = (
-	req: Request,
-	res: Response,
-	next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-	try {
-		// if auth is not enabled, render the not available page
-		if (!isFeatureEnabled(config.FEATURE_FLAG_ENABLE_AUTH)) {
-			log.infoRequest(req, "sorry, auth service not available right now");
-			return res.render(config.NOT_AVAILABLE);
-		}
+    try {
+        // if auth is not enabled, render the not available page
+        if (!isFeatureEnabled(config.FEATURE_FLAG_ENABLE_AUTH)) {
+            log.infoRequest(req, 'sorry, auth service not available right now');
+            return res.render(config.NOT_AVAILABLE);
+        }
 
-		// If auth enabled
-		log.infoRequest(req, "some auth here soon!");
+        // If auth enabled
+        log.infoRequest(req, 'some auth here soon!');
 
-		next();
-	} catch (err: any) {
-		log.errorRequest(req, err);
-		next(err);
-	}
+        next();
+    } catch (err: any) {
+        log.errorRequest(req, err);
+        next(err);
+    }
 };
+
--- test/unit/middleware/authentication.middleware.spec.ts
+++ test/unit/middleware/authentication.middleware.spec.ts
@@ -1,23 +1,21 @@
-/* eslint-disable indent */
-/* eslint-disable @typescript-eslint/quotes */
-jest.mock("../../../src/utils/isFeatureEnabled");
-jest.mock("../../../src/utils/logger", () => ({
-	log: {
-		infoRequest: jest.fn(),
-		errorRequest: jest.fn(),
-	},
+jest.mock('../../../src/utils/isFeatureEnabled');
+jest.mock('../../../src/utils/logger', () => ({
+    log: {
+        infoRequest: jest.fn(),
+        errorRequest: jest.fn(),
+    },
 }));
 
-import { describe, expect, test, jest, afterEach } from "@jest/globals";
-import { Request, Response, NextFunction } from "express";
+import { describe, expect, test, jest, afterEach } from '@jest/globals';
+import { Request, Response, NextFunction } from 'express';
 
-import * as config from "../../../src/config";
+import * as config from '../../../src/config';
 
-import { authentication } from "../../../src/middleware/authentication.middleware";
-import { log } from "../../../src/utils/logger";
-import { isFeatureEnabled } from "../../../src/utils/isFeatureEnabled";
+import { authentication } from '../../../src/middleware/authentication.middleware';
+import { log } from '../../../src/utils/logger';
+import { isFeatureEnabled } from '../../../src/utils/isFeatureEnabled';
 
-import { GET_REQUEST_MOCK } from "../../mock/data";
+import { GET_REQUEST_MOCK } from '../../mock/data';
 
 const logInfoRequestMock = log.infoRequest as jest.Mock;
 const logErrorRequestMock = log.errorRequest as jest.Mock;
@@ -25,70 +23,70 @@ const isFeatureEnabledMock = isFeatureEnabled as jest.Mock;
 
 const req = GET_REQUEST_MOCK as Request;
 const mockResponse = () => {
-	const res = {} as Response;
-	res.render = jest.fn() as any;
-	return res;
+    const res = {} as Response;
+    res.render = jest.fn() as any;
+    return res;
 };
 const next = jest.fn() as NextFunction;
 
-describe("Authentication Middleware test suites", () => {
-	let res: any;
+describe('Authentication Middleware test suites', () => {
+    let res: any;
 
-	beforeEach(() => {
-		res = mockResponse();
-	});
+    beforeEach(() => {
+        res = mockResponse();
+    });
 
-	afterEach(() => {
-		jest.resetAllMocks();
-	});
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
 
-	test("Should call next if auth feature is enabled", () => {
-		// enable auth flag
-		isFeatureEnabledMock.mockReturnValue(true);
+    test('Should call next if auth feature is enabled', () => {
+        // enable auth flag
+        isFeatureEnabledMock.mockReturnValue(true);
 
-		authentication(req, res, next);
+        authentication(req, res, next);
 
-		expect(logInfoRequestMock).toHaveBeenCalledTimes(1);
-		expect(logInfoRequestMock).toHaveBeenCalledWith(
-			req,
-			"some auth here soon!"
-		);
-		expect(next).toHaveBeenCalledTimes(1);
-	});
+        expect(logInfoRequestMock).toHaveBeenCalledTimes(1);
+        expect(logInfoRequestMock).toHaveBeenCalledWith(
+            req,
+            'some auth here soon!'
+        );
+        expect(next).toHaveBeenCalledTimes(1);
+    });
 
-	test("should call res.render with not-available view if auth feature is disabled", () => {
-		// disable auth flag
-		isFeatureEnabledMock.mockReturnValue(false);
+    test('should call res.render with not-available view if auth feature is disabled', () => {
+        // disable auth flag
+        isFeatureEnabledMock.mockReturnValue(false);
 
-		authentication(req, res, next);
+        authentication(req, res, next);
 
-		expect(logInfoRequestMock).toHaveBeenCalledTimes(1);
-		expect(logInfoRequestMock).toHaveBeenCalledWith(
-			req,
-			"sorry, auth service not available right now"
-		);
-		expect(res.render).toHaveBeenCalledTimes(1);
-		expect(res.render).toHaveBeenCalledWith(config.NOT_AVAILABLE);
-	});
+        expect(logInfoRequestMock).toHaveBeenCalledTimes(1);
+        expect(logInfoRequestMock).toHaveBeenCalledWith(
+            req,
+            'sorry, auth service not available right now'
+        );
+        expect(res.render).toHaveBeenCalledTimes(1);
+        expect(res.render).toHaveBeenCalledWith(config.NOT_AVAILABLE);
+    });
 
-	test("should call next with error object if error is thrown", () => {
-		// disable auth flag so res.render is called
-		isFeatureEnabledMock.mockReturnValue(false);
+    test('should call next with error object if error is thrown', () => {
+        // disable auth flag so res.render is called
+        isFeatureEnabledMock.mockReturnValue(false);
 
-		// force an error to be thrown by res.render
-		const errMsg = "Error thrown by res.render!";
-		const resWithError = { ...res };
-		resWithError.render = jest.fn(() => {
-			throw new Error(errMsg);
-		});
+        // force an error to be thrown by res.render
+        const errMsg = 'Error thrown by res.render!';
+        const resWithError = { ...res };
+        resWithError.render = jest.fn(() => {
+            throw new Error(errMsg);
+        });
 
-		authentication(req, resWithError, next);
+        authentication(req, resWithError, next);
 
-		const errObj = expect.objectContaining({ message: errMsg });
+        const errObj = expect.objectContaining({ message: errMsg });
 
-		expect(logErrorRequestMock).toHaveBeenCalledTimes(1);
-		expect(logErrorRequestMock).toHaveBeenCalledWith(req, errObj);
-		expect(next).toHaveBeenCalledTimes(1);
-		expect(next).toHaveBeenCalledWith(errObj);
-	});
+        expect(logErrorRequestMock).toHaveBeenCalledTimes(1);
+        expect(logErrorRequestMock).toHaveBeenCalledWith(req, errObj);
+        expect(next).toHaveBeenCalledTimes(1);
+        expect(next).toHaveBeenCalledWith(errObj);
+    });
 });
