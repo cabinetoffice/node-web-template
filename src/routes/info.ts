6d9d13e90f428618c3c775749748ec94dfe1d491
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-07-09 17:26:26 +0100

    mount authentication mw onto info route

--- src/routes/info.ts
+++ src/routes/info.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 
 import { get, post } from '../controller/info.controller';
 import * as config from '../config';
+import { authentication } from '../middleware/authentication.middleware';
 
 const infoRouter = Router();
 
-infoRouter.get(config.INFO_URL, get);
+infoRouter.get(config.INFO_URL, authentication, get);
 infoRouter.post(config.INFO_URL, post);
 
 export default infoRouter;
