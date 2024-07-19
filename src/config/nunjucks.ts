fd40527720fd49474b2897c4ebef4250d133e51c
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2024-07-19 11:53:13 +0100

    update govuk scripts to use esm modules and update scriptsrc shas

--- src/config/nunjucks.ts
+++ src/config/nunjucks.ts
@@ -8,7 +8,7 @@ export const configureNunjucks = (app: express.Application, viewsPath: string) =
     log.info(`Set nunjucks configurations and where nunjucks templates should resolve to: ${viewsPath}`);
 
     const nunjucksEnv = nunjucks.configure(
-        [viewsPath, 'node_modules/govuk-frontend', 'node_modules/govuk-frontend/components'],
+        [viewsPath, 'node_modules/govuk-frontend/dist', 'node_modules/govuk-frontend/dist/components'],
         {
             autoescape: true,
             express: app
